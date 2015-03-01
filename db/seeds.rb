eb65c921a187ef800a6f3949f3b9f53748ed5aa5
aparmer1 <aparmer1@my.westga.edu>
2015-03-01 12:46:36 -0800

    List and Show pages use hyperlink on username if url is provided for the ripple.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,37 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+#This file contains the data used to populate the database...
+
+Ripple.delete_all
+#...
+Ripple.create!(username: 'kuyal8784',
+  username_url: 'http://www.battle.net',
+  message: "World of Warcraft is the BEST video game ever created! You can create
+  any type of character that you want to, and there are many different 		
+  classes (like Priest, Mage, Warlock, Warrior, etc.). You can compete 		
+  on a battleground, or journey through a dungeon with you WoW friends. 		
+  I don't know why anyone WOULDN'T play WoW...")
+#...
+Ripple.create!(username: 'mario123',
+  username_url: 'http://www.nintendo.com',
+  message:
+  "It's a me! Mario! Luigi and I are really hoping that Nintendo will
+  come out with a new game for us to be in...We should all write them
+  emails to convince this to happen!!")
+#...
+Ripple.create!(username: 'nerdyGirl00',
+  message:
+  "I just wanted to post and vent really quick... Why is it that people always 
+  think girls who play video games are nerds? I just don't understand! People I 
+  go to school with play sports, or do ballet. What's wrong with spending my 
+  free time playing games with my brother? I see no problem with it. 
+  What do you think?")
+#...
+Ripple.create!(username: 'MoNeYmAkEr',
+  username_url: 'http://www.makeuseof.com/tag/6-ways-to-actually-make-money-playing-video-games/',
+  message: 
+  "Are you tired of not having any money? Would you like to get PAID for doing 
+  things that you already enjoy doing? Look no further! Click on my username to 
+  find out 6 ways you can actually make money playing video games!")
+#...
+
+
+
